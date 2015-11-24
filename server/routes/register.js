f477e260d454fe08d8ac549d90353925473772ce
jasontenbrink <jason.tenbrink@gmail.com>
2015-11-23 23:41:25 -0600

    can now put a hashed password in the DB

--- server/routes/register.js
+++ server/routes/register.js
@@ -3,8 +3,9 @@ var router = express.Router();
 var passport = require('passport');
 var path = require('path');
 var Users = require('../models/user');
-var pg  = require('pg ');
-var bcrypt = require('bcrypt');
+var pg  = require('pg');
+var Promise = require('bluebird');
+var bcrypt = Promise.promisifyAll(require('bcrypt'));
 var SALT_WORK_FACTOR = 10;
 
 
@@ -15,46 +16,33 @@ router.get('/', function (req, res, next){
 });
 
 router.post('/', function(req,res,next){
-
   var user = req.body;
 
 //  if(!user.isModified('password')) return next;
 
-  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
-      if(err) return next(err);
 
-      bcrypt.hash(user.password, salt, function(err, hash){
-          if(err) return next(err);
+    bcrypt.genSaltAsync(SALT_WORK_FACTOR).then(function(salt){
+      //  if(err) return next(err);
+        console.log('value of salt, before hash', salt);
+        console.log('value of pwd before hash', user.password);
+        return bcrypt.hashAsync(user.password, salt);
+      })
+      .then(function(hash){
+         user.password = hash;
+            console.log('pwd from inside bcrypt after hash', user.password);
+            //next();
+            pg.connect(connectionString, function (err, client, done) {
+              if (err) console.log(err);
+              console.log('pwd from just before DB write', user);
+              client.query('insert into people (email, password) VALUES ($1, $2)',
+                  [req.body.username, req.body.password],
+                  function (err, res) {
+                    if (err) console.log(err);
 
-          user.password = hash;
-          next();
+                  });
+                  res.redirect('/');
+            });
       });
-  });
-
-
-  pg.connect(connectionString, function (err, client, done) {
-    if (err) console.log(err);
-    client.query('insert into people (email, password) VALUES ($1, $2)',
-        [req.body.username, req.body.password],
-        function (err, res) {
-          if (err) console.log(err);
-          else  res.redirect('/');
-        });
-  });
-
-
-  // (err, client, done) {
-  //   var query = client.query('SELECT * FROM people WHERE' +
-  //                             ' first_name ILIKE $1 AND last_name ILIKE $2' +
-  //                             ' AND email ILIKE $3',
-  //    [firstNameParam, lastNameParam, emailParam]);
-    // Users.create(req.body, function(err,post){
-    //     if(err){
-    //         next(err);
-    //     } else {
-    //         res.redirect('/');
-    //     }
-    // }) ;
 });
 
 module.exports = router;
