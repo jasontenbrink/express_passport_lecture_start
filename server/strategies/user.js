814fb56aa0c28d636f8c9cd8ee1f94cf1fed02ee
jasontenbrink <jason.tenbrink@gmail.com>
2015-11-24 16:39:44 -0600

    authentication works!

--- server/strategies/user.js
+++ server/strategies/user.js
@@ -7,58 +7,63 @@ var bcrypt = require('bcrypt');
 
 var connectionString = process.env.DATABASE_URL   || 'postgres://localhost:5432/church';
 passport.serializeUser(function(user, done){
-  console.log('Serialize, what is the value of username', user.username);
+  console.log('Serializer, what is the value of username', user.username);
 
     done(null, user.username);
 });
 
 passport.deserializeUser(function(id, done){
-  console.log('deserialize, is this getting called inside deserializeUser?');
+  console.log('is this getting called inside deserializeUser?');
+  console.log('deserialize id is: ', id);
   //find a user and then say done(null, user)
-  pg.connect(connectionString, function (err, client, done) {
-    client.query("select username, password from people where pin = $1", [id],
-    function (err, response) {
-      user = response;
-      done(null, user);
-      });
-    });
-
+  pg.connect(connectionString, function (err, client) {
+    client.query("select username, password from people where username = $1", [id],
+  function (err, response) {
+    console.log('this is the response in deserializer', response);
+    username = response.rows[0].username;
+    client.end();
+    done(null, username);
+  });
 });
 
-
+});
 
 passport.use('local', new localStrategy({
     passReqToCallback: true,
     usernameField: 'username' //might need to be email
     }, function(req, username, password, done) {
       //make DB call to get userspassword. on the post body.
-    console.log('this is the req body from the strat, newb', req.body);
+    console.log('right before the DB call, req.body', req.body);
 
-    pg.connect(connectionString, function (err,client, done) {
+    pg.connect(connectionString, function (err,client) {
+      //send pasword down to DB as well and pull out the info, that way we don't pull
       client.query("select password, pin from people where username = 'jasont2'",
       function (err, response) {
         var dbPassword = response.rows[0].password;
+        client.end();
         console.log('the password from the DB', dbPassword);
-        console.log('password from login', req.body.password);
+        //console.log('password from login', req.body.password);
+          console.log('req.body after sql call', req.body);
 
             bcrypt.compare(req.body.password, dbPassword, function(err, isMatch){
-                if(err) return cb(err);
+                if(err) return err;
                 //cb(null, isMatch);
               //  console.log('this is the incoming password.  IS it hashed?', );
                 console.log('isMatch value from compare', isMatch);
+
                 if (isMatch){
-                  //console.log('just before done');
+                  //console.log(done);
                   //return done(null, req.body);
                   return done(null, req.body);
                   //close connection
                 }
+                else{
+                  return done(null, false, {message:'failed'});
+                }
               //  console.log('does done 2 get seen?');
                 //return done(null, req.body);
-                return done (null, req.body);
             });
           //  client.end();
-          done(null, true);
-
       });
 
     });
@@ -67,3 +72,21 @@ passport.use('local', new localStrategy({
 }));
 
 module.exports = passport;
+
+// function(candidatePassword, cb){
+//     bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+//         if(err) return cb(err);
+//         cb(null, isMatch);
+//     });
+        // User.findOne({username: username}, function (err, user) {
+        //     if (err) throw err;
+        //     if (!user) return done(null, false, {message: 'Incorrect username and password'});
+        //     user.comparePassword(password, function (err, isMatch) {
+        //         if (err) throw err;
+        //         if (isMatch) {
+        //             return done(null, user);
+        //         } else {
+        //             done(null, false, {message: 'Incorrect username and password'});
+        //         }
+        //     });
+        // });
